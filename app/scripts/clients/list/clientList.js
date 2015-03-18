a0f19f27e6d5ad141dd93a6e881ea0665c835d76
Abiee Alejandro Echamea Márquez <abiee.alejandro@gmail.com>
2015-03-17 22:01:05 -0700

    Show an empty placeholder when clients resource is empty

--- app/scripts/clients/list/clientList.js
+++ app/scripts/clients/list/clientList.js
@@ -1,6 +1,7 @@
 import Marionette from 'backbone.marionette';
 import ListLayout from './listLayout';
 import ClientListView from './clientListView';
+import EmptyListView from './emptyListView';
 
 export default class ClientList extends Marionette.Object {
   constructor(options) {
@@ -8,9 +9,14 @@ export default class ClientList extends Marionette.Object {
   }
 
   showList(clients) {
-    var layout = new ListLayout();
-    var view = new ClientListView({ collection: clients });
-    this.region.show(layout);
-    layout.getRegion('list').show(view);
+    if (clients.length === 0) {
+      var view = new EmptyListView();
+      this.region.show(view);
+    } else {
+      var layout = new ListLayout();
+      var view = new ClientListView({ collection: clients });
+      this.region.show(layout);
+      layout.getRegion('list').show(view);
+    }
   }
 }
---
+++ app/scripts/clients/list/emptyListView.js
@@ -0,0 +1,11 @@
+import Marionette from 'backbone.marionette';
+import emptyListViewTemplate from 'clients/list/templates/emptyListView';
+
+export default class EmptyListView extends Marionette.ItemView {
+  constructor(...rest) {
+    this.template = emptyListViewTemplate;
+    this.className = 'col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 ' +
+                     'text-center';
+    super(...rest);
+  }
+}
